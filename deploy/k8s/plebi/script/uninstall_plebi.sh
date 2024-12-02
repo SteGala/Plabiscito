6eada346b516c2c83818bb1edbce62d3084b4c6b
SteGala <galaxx.ste@gmail.com>
2024-12-02 18:41:35 +0100

    update

--- deploy/k8s/plebi/script/uninstall_plebi.sh
+++ deploy/k8s/plebi/script/uninstall_plebi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl delete all -n plebi --all
+kubectl delete deployment -n offloaded-namespace --all
 kubectl delete all -n offloaded-namespace --all
 kubectl delete -f permissions.yml
 kubectl delete namespace plebi
