d2c3ff13c7bad521fb2b7e5f5effd769111a94e8
SteGala <galaxx.ste@gmail.com>
2024-12-04 10:08:38 +0100

    update

--- deploy/k8s/plebi/script/install_requirements.sh
+++ deploy/k8s/plebi/script/install_requirements.sh
@@ -31,6 +31,7 @@ kubectl apply -f manifests/
 kubectl delete networkpolicies --all -n monitoring
 kubectl patch service grafana -n monitoring -p '{"spec": {"type": "NodePort"}}'
 kubectl patch service prometheus-k8s -n monitoring -p '{"spec": {"type": "NodePort"}}'
+kubectl patch service node-exporter -n monitoring -p '{"spec": {"type": "NodePort"}}'
 
 echo "kube-prometheus installed successfully."
 cd ..
