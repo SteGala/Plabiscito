dd61dbb1d110c7c86f91004fde6706f1313b7a13
SteGala <galaxx.ste@gmail.com>
2025-05-06 20:43:41 +0200

    update plot

--- res/plot.py
+++ res/plot.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as mat
 import re
 import seaborn as sns
 import numpy as np
@@ -61,9 +61,9 @@ def plot_failure_severity(base_path, file_id=0, filename=None):
     # Plot
     sns.barplot(data=df, x="Algorithm", y="Failed applications", estimator="mean", errorbar=None, color="red", alpha=0.5)
     sns.stripplot(data=df, x="Algorithm", y="Failed applications", color="black", size=6, jitter=False)
-    plt.tight_layout()
-    plt.savefig(filename)  
-    plt.clf()  # Clear the figure for the next plot
+    mat.pyplot.tight_layout()
+    mat.pyplot.savefig(filename)  
+    mat.pyplot.clf()  # Clear the figure for the next plot
 
 def plot_bandwidth_boxplots(base_path, file_id=0, filename=None):
     all_data = []
@@ -85,17 +85,15 @@ def plot_bandwidth_boxplots(base_path, file_id=0, filename=None):
     # Convert to DataFrame for Seaborn
     plot_df = pd.DataFrame(all_data)
 
-    #plt.figure(figsize=(12, 6))
+    #mat.pyplot.figure(figsize=(12, 6))
     sns.violinplot(x='Algorithm', y='Bandwidth', data=plot_df, inner='box', cut=0)
-    plt.ylabel("Network Traffic (Gb/s)")
-    plt.grid(True, linestyle="--", alpha=0.5)
-    plt.tight_layout()
-    plt.savefig(filename)
-    plt.clf()  # Clear the figure for the next plot
+    mat.pyplot.ylabel("Network Traffic (Gb/s)")
+    mat.pyplot.grid(True, linestyle="--", alpha=0.5)
+    mat.pyplot.tight_layout()
+    mat.pyplot.savefig(filename)
+    mat.pyplot.clf()  # Clear the figure for the next plot
 
 def plot_cdfs_from_directories(base_path, file_id=0, file_name=None):
-    plt.figure(figsize=(10, 6))
-
     for dir_name in sorted(os.listdir(base_path)):
         dir_path = os.path.join(base_path, dir_name)
         if os.path.isdir(dir_path):
@@ -116,19 +114,24 @@ def plot_cdfs_from_directories(base_path, file_id=0, file_name=None):
             cdf = np.arange(1, len(sorted_vals) + 1) / len(sorted_vals)
 
             # Plot CDF
-            plt.plot(sorted_vals, cdf, label=dir_name)
+            mat.pyplot.plot(sorted_vals, cdf, label=dir_name)
 
-    plt.xlabel("Training Time (s)")
-    plt.ylabel("CDF")
-    plt.legend(title="Algorithms", bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.grid(True, linestyle="--", alpha=0.6)
-    plt.tight_layout()
-    plt.savefig(file_name)
-    plt.clf()  # Clear the figure for the next plot
+    mat.pyplot.xlabel("Training Time (s)")
+    mat.pyplot.ylabel("CDF")
+    mat.pyplot.legend(title="Algorithms")
+    mat.pyplot.grid(True, linestyle="--", alpha=0.6)
+    mat.pyplot.tight_layout()
+    mat.pyplot.savefig(file_name)
+    mat.pyplot.clf()  # Clear the figure for the next plot
 
-plot_failure_severity(".", 0, "failure_severity.png")
-plot_bandwidth_boxplots(".", 1, "rx_bandwidth_interface.png")
-plot_bandwidth_boxplots(".", 2, "rx_bandwidth_localhost.png")
-plot_bandwidth_boxplots(".", 3, "tx_bandwidth_interface.png")
-plot_bandwidth_boxplots(".", 4, "tx_bandwidth_localhost.png")
-plot_cdfs_from_directories(".", 5, "training_time.png")
+mat.pyplot.figure(figsize=(6, 3))
+font = {"size": 14}
+
+mat.rc('font', **font)
+
+plot_failure_severity(".", 0, "failure_severity.pdf")
+plot_bandwidth_boxplots(".", 1, "rx_bandwidth_interface.pdf")
+plot_bandwidth_boxplots(".", 2, "rx_bandwidth_localhost.pdf")
+plot_bandwidth_boxplots(".", 3, "tx_bandwidth_interface.pdf")
+plot_bandwidth_boxplots(".", 4, "tx_bandwidth_localhost.pdf")
+plot_cdfs_from_directories(".", 5, "training_time.pdf")
